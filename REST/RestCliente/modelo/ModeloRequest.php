f239376c3830bdeb7718dad9efb45971ca186731
Roberto Albornoz <54447132+Riserek@users.noreply.github.com>
2020-05-06 02:22:14 -0400

    Arreglado un pequeño detalle que no se guardo

--- REST/RestCliente/modelo/ModeloRequest.php
+++ REST/RestCliente/modelo/ModeloRequest.php
@@ -2,11 +2,6 @@
 
 	class CurlRequest{
 		public function nombreSendPost(){
-			//$nombre = $_POST['nombre'];
-			//$apellido_p = $_POST['apellido_p'];
-			//$apellido_m = $_POST['apellido_m'];
-			//$sexo = $_POST['sexo'];
-
 			$data = array(
 						"nombre" => $_POST['nombre'],
 						"apellido_p" => $_POST['apellido_p'],
@@ -33,19 +28,12 @@
 		}
 		
 	public function rutSendPost(){
-			//$rut = $_POST['rut'];
             $data = array("rut" => $_POST['rut']);
-            //url de la API verificar rut
             $ch = curl_init("http://localhost:4000/rut");
-            //true si es correcto, false si no lo es
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            //establecemos el verbo http que queremos utilizar para la petición
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            //enviamos el array data
             curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-            //obtenemos la respuesta
             $response = curl_exec($ch);
-            // Se cierra el recurso CURL y se liberan los recursos del sistema
             curl_close($ch);
             if(!$response) {
                 return false;
