7ac812ff9eda0927b12957b1649dd9102f1d4482
alexbidartsomta <alexbidart@live.com>
2020-05-09 23:07:52 -0400

    Ultima actualizacion

--- SOAP/SoapCliente/controlador/ControladorRequest.php
+++ SOAP/SoapCliente/controlador/ControladorRequest.php
@@ -3,12 +3,12 @@
 		public function rutSendPost()
 		{
 			$rut = $_POST['rut']; //guarda el valor obtenido en el string direccion
-        	$servicio='http://localhost:1212/WS/Servicioweb?wsdl'; 
-        	$parametros=array('arg0'=>$rut);
+        	$servicio='http://192.168.0.10:8080/SoapService/services/SoapService?wsdl'; 
+        	$parametros=array('rut'=>$rut);
         	$client=new SoapClient($servicio);
 			$result=$client->validarrut($parametros);
     		print "<div class=\"form-group px-5 shadow p-3 mb-5 bg-white rounded\">
-			<p>".$rut." es un ".$result->return."</p> </div>";
+			<p>".$rut." es un ".$result->validarrutReturn."</p> </div>";
 		}
 		
 		public function nombreSendPost(){
@@ -17,12 +17,12 @@
             $apellidopa=$_POST['apellidopa'];
             $apellidoma=$_POST['apellidoma'];
             $sexo=$_POST['sexo'];
-            $servicio2='http://localhost:1212/WS/Servicioweb?wsdl'; 
-            $parametros2=array('arg0'=>$nombres,'arg1'=>$apellidopa,'arg2'=>$apellidoma,'arg3'=>$sexo);
+            $servicio2='http://192.168.0.10:8080/SoapService/services/SoapService?wsdl'; 
+            $parametros2=array('nombres'=>$nombres,'apellidopa'=>$apellidopa,'apellidoma'=>$apellidoma,'sexo'=>$sexo);
             $client2=new SoapClient($servicio2);
             $result2=$client2->nombrepropio($parametros2);
 			print "<div class=\"form-group px-5 shadow p-3 mb-5 bg-white rounded \">
-			<p class=\"text-center\"> Hola ".$result2->return."</p> </div>"; 
+			<p class=\"text-center\"> Hola ".$result2->nombrepropioReturn."</p> </div>"; 
 	
 	}
 }
